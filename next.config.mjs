9ece88a09a6876b3b585b41bb36c6deddd18820b
zshuhanzz <shuhan.zhang@student.tdsb.on.ca>
2026-03-25 22:26:09 -0400

    added github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/website_2",
+  assetPrefix: "/website_2/",
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
